f43549d04f84c1b78839e8e862c9c377515c6f78
Justine Lam <justine.lam@gmail.com>
2016-03-24 15:17:45 -0700

    Add templates for including different quicklinks for each type of page

--- template-absentee.php
+++ template-absentee.php
@@ -8,4 +8,5 @@
 <?php while (have_posts()) : the_post(); ?>
 
   <?php get_template_part('templates/content', 'absentee'); ?>
+  <?php get_template_part('templates/content', 'quicklinks-absentee'); ?>
 <?php endwhile; ?>
--- template-state.php
+++ template-state.php
@@ -8,4 +8,5 @@
 <?php while (have_posts()) : the_post(); ?>
 
   <?php get_template_part('templates/content', 'state'); ?>
+  <?php get_template_part('templates/content', 'quicklinks-state'); ?>
 <?php endwhile; ?>
