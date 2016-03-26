8a7da16a1ebbf86e6734a818b027408d782a5050
Justine Lam <justine.lam@gmail.com>
2016-03-25 17:17:58 -0700

    Update to addthis js

--- base.php
+++ base.php
@@ -34,8 +34,23 @@ use Roots\Sage\Wrapper;
       wp_footer();
 
     ?>
-    <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56bd6f489abb811d"></script>
+    <script type="text/javascript">
+    var addthis_config =
+    {
+      // enable social tracking
+      data_ga_social : true,
+      // enable clickback tracking
+      data_track_clickback: true,
+      // address bar tracking for homepage
+      data_track_addressbar_paths: '/*',
+      // address bar tracking
+      data_track_addressbar: true,
+      //make drop-down show up in correct place
+      ui_offset_top: '20px',
+      ui_offset_left: '20px'
+    }
+    </script>
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56bd6f489abb811d" async="async"></script>
 
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
