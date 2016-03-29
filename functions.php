f6d649d4c5105587cc2864794793c1fd7ed285da
Justine Lam <justine.lam@gmail.com>
2016-03-29 13:01:10 -0700

    WP Admin - order state posts alphabeticallly by default

--- functions.php
+++ functions.php
@@ -28,6 +28,14 @@ foreach ($sage_includes as $file) {
 }
 unset($file, $filepath);
 
+function set_post_order_in_admin( $wp_query ) {
+global $pagenow;
+  if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
+    $wp_query->set( 'orderby', 'title' );
+    $wp_query->set( 'order', 'ASC' );
+  }
+}
+add_filter('pre_get_posts', 'set_post_order_in_admin' );
 
 
 // function createStates() {
