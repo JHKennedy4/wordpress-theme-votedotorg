7512eb50802f7b894e268a7999b9a62f4290eaa7
Justine Lam <justine.lam@gmail.com>
2016-04-01 16:37:45 -0700

    Styling the footer to use |'s to separate out Terms, Privacy Policy & Contact Us
    Moving legalese and links closer together, removing other margins and padding

--- templates/footer.php
+++ templates/footer.php
@@ -31,7 +31,7 @@
         <li><a href="/about">About</a></li>
         <li><a href="/team">Team</a></li>
         <li><a href="http://www.facebook.com/longdistancevoter">Facebook</a></li>
-        <li><a href="http://twitter.com/intent/follow?source=followbutton&variant=1.0&screen_name=absenteeballots">Twitter</a></li>
+        <li><a href="http://twitter.com/intent/follow?source=followbutton&variant=1.0&screen_name=votedotorg">Twitter</a></li>
         <li><a href="http://votedotorg.tumblr.com">Tumblr</a></li>
       </ul>
     </div><!--.nav-links-->
