58d1dadb96a573e0ff604b93e092ba96bc14687d
Justine Lam <justine.lam@gmail.com>
2016-04-01 01:24:44 -0700

    Add default sharing meta tags for FB and Twitter.
    - Add all share images

--- templates/head.php
+++ templates/head.php
@@ -1,3 +1,5 @@
+
+
 <head>
 
    <meta charset="utf-8">
@@ -6,6 +8,20 @@
    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon.ico">
 
    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/styles/uswds.css">
+   <!--All the Meta Tags -->
+   <title> <?php $title = get_the_title(); if (!empty($title)) { echo $title." | "; }  ?>Vote.org - Everything you need to vote</title>
+
+   <meta property="og:title" content="<?php wp_title(); ?> | Vote.org - Everything you need to vote">
+   <meta name="description" content="Vote.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient."/>
+   <meta property="og:url" content="<?php echo home_url();?>">
+   <meta property="og:type" content="article">
+   <meta property="og:site_name" content="Vote.org">
+   <meta property="og:image" content="<?php $template_dir = get_template_directory_uri(); echo $template_dir.'/dist/images/og-default.jpg';?>">
+
+   <meta name="twitter:site" content="@votedotorg">
+   <meta name="twitter:url" content="<?php echo home_url();?>">
+   <meta name="twitter:card" content="summary_large_image">
+
 
    <!--[if lt IE 9]>    
    <script src="./js/html5shiv.js"></script>
