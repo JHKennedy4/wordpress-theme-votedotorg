0fd4a2be0d009939644bc70cc7846e9049888f72
Justine Lam <justine.lam@gmail.com>
2016-04-07 14:34:22 -0700

    Hide header nav in tablet and above for state elction center pages

--- base.php
+++ base.php
@@ -21,7 +21,7 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <?php if( is_home() || is_front_page() ) : 
+    <?php if( is_home() || is_front_page() || is_page( $page = "states") ) : 
     do_action('get_header');
      get_template_part('templates/header-home');
    
