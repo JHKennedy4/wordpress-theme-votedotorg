7a66a5522ea6e5bed549893c9e3cc76da6e7d072
Justine Lam <justine.lam@gmail.com>
2016-04-07 18:02:36 -0700

    Add class for state election center h1 element
    - Style change: Move “Everything you need to vote” closer to “California Election Center”; distance can be ~20px

--- templates/content-hero.php
+++ templates/content-hero.php
@@ -27,7 +27,7 @@ if (isset($wp_query->query_vars['state_name'])) {
     <h2 class="logo">Vote.org</h2>
     <?php if ($state_name !== "") { ?>
 
-    <h1><?php echo $state_name;?> Election Center</h1>
+    <h1 class="state-election-center"><?php echo $state_name;?> Election Center</h1>
     <div class="tagline">Everything you need to vote</div>
     <?php }  else { ?> 
     <h1>Everything you need to vote</h1>
