f2b602d30cc92eaeaf0bbd8dd857dfa61caa2f86
Justine Lam <justine.lam@gmail.com>
2016-04-11 17:26:37 -0700

    Change header in helpful links section to <h2>

--- templates/content-links.php
+++ templates/content-links.php
@@ -1,5 +1,5 @@
 
-<h3><?php $state_name; ?> Helpful voting links</h3>
+<h2><?php $state_name; ?> Helpful voting links</h2>
 
 <ul>
   <li><a href="<?php echo types_render_field('state-election-website', array('raw' => true));?>">State Election Website</a></li>
