0deebc33300526b6e1c23d4668b3fdd0f65be7df
Justine Lam <justine.lam@gmail.com>
2016-04-18 14:23:35 -0700

    Add conditionals for all Helpful Links to only display if it isn't empty

--- templates/content-links.php
+++ templates/content-links.php
@@ -1,16 +1,38 @@
 
 <h2><?php $state_name; ?> Helpful voting links</h2>
-
+<?php
+ $state_election_site = types_render_field('state-election-website', array('raw' => true));
+ $leo_link = types_render_field('local-election-official', array('raw' => true));
+ $polling_locator = types_render_field('polling-place-locator', array('raw' => true));
+ $online_ballot_tracker = types_render_field('online-ballot-tracker-url', array('raw' => true));
+ $absentee_ballot_url = types_render_field('absentee-ballot-info-url', array('raw' => true));
+ $early_voting_url = types_render_field('early-voting-info-url', array('raw' => true));
+ $voter_id_info = types_render_field('voter-id-more-information', array('raw' => true)); 
+ ?>
 <ul>
-  <li><a href="<?php echo types_render_field('state-election-website', array('raw' => true));?>">State Election Website</a></li>
-  
-  <li><a href="<?php echo types_render_field('local-election-official', array('raw' => true));?>">Local Election Officials</a>: These are the best people to contact if you have any questions at all about voting in your state.</li>
-  <li><a href="<?php echo types_render_field('polling-place-locator', array('raw' => true));?>">Find your polling place</a></li>
-  <li><a href="<?php echo types_render_field('online-ballot-tracker-url', array('raw' => true));?>">Absentee ballot tracker tool</a></li>
-  <li><a href="<?php echo types_render_field('absentee-ballot-info-url', array('raw' => true));?>">Learn more about absentee voting</a></li>
+  <?php if (!empty($state_election_site) ) {  ?>
 
-  <li><a href="<?php echo types_render_field('early-voting-info-url', array('raw' => true));?>">Learn more about early voting</a></li>
-  <li><a href="<?php echo types_render_field('voter-id-more-information', array('raw' => true));?>">Learn more about voter ID </a></li>
+  <li><a href="<?php echo $state_election_site; ?>">State Election Website</a></li>
+  <?php } ?>
+
+  <?php if (!empty($leo_link) ) {  ?>
+  <li><a href="<?php echo $leo_link; ?>">Local Election Officials</a>: These are the best people to contact if you have any questions at all about voting in your state.</li>
+  <?php } ?>
+  <?php if (!empty($polling_locator) ) {  ?>
+  <li><a href="<?php echo $polling_locator ?>">Find your polling place</a></li>
+  <?php } ?>
+  <?php if (!empty($online_ballot_tracker) ) {  ?>
+  <li><a href="<?php echo $online_ballot_tracker; ?>">Absentee ballot tracker tool</a></li>
+  <?php } ?>
+  <?php if (!empty($absentee_ballot_url) ) {  ?>
+  <li><a href="<?php echo $absentee_ballot_url; ?>">Learn more about absentee voting</a></li>
+  <?php } ?>
+  <?php if (!empty($early_voting_url) ) {  ?>
+  <li><a href="<?php echo $early_voting_url;?>">Learn more about early voting</a></li>
+  <?php } ?>
+  <?php if (!empty($voter_id_info) ) {  ?>
+  <li><a href="<?php echo $voter_id_info;?>">Learn more about voter ID </a></li>
+  <?php } ?>
 
 
 </ul>
