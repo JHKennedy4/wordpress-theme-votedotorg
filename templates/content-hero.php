b78e24f8615dcd18f0233771bb7ff0dc1bd388bc
Justine Lam <justine.lam@gmail.com>
2016-04-18 14:02:25 -0700

    Change Vote.org to VOTE.org for hero section on homepage and state pages

--- templates/content-hero.php
+++ templates/content-hero.php
@@ -24,7 +24,7 @@ if (isset($wp_query->query_vars['state_name'])) {
 ?>
 <section class="hero">
   <div class="container">
-    <h2 class="logo">Vote.org</h2>
+    <h2 class="logo">VOTE.org</h2>
     <?php if ($state_name !== "") { ?>
 
     <h1 class="state-election-center"><?php echo $state_name;?> Election Center</h1>
