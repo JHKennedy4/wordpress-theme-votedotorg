774bb54119eb21449469de33017a3bfc28594513
Justine Lam <justine.lam@gmail.com>
2016-04-21 13:04:26 -0700

    Update all meta tags from Vote.org to VOTE.org

--- lib/meta.php
+++ lib/meta.php
@@ -27,20 +27,20 @@ function hook_meta() {
   //define $title for Register to Vote 
   if ($page_type == "register-to-vote" && $state == null ) {
     
-    $title = "Register to Vote | Vote.org - The Voter Registration Experts";
+    $title = "Register to Vote | VOTE.org - The Voter Registration Experts";
     $description = "It takes 2 minutes to register to vote. Get started now.";
     $image = $theme_uri."/dist/images/og-register-to-vote.jpg";
   } 
   //define $title for Register to Vote State Specific Pages
   else if ($page_type == "register-to-vote" && $state !== "") {
     $state_name = $post->post_title;
-    $title = "Register to Vote in $state_name | Vote.org - The Voter Registration Experts";
+    $title = "Register to Vote in $state_name | VOTE.org - The Voter Registration Experts";
     $description = "It takes 2 minutes to register to vote in $state_name. Get started now.";
     $image = $theme_uri."/dist/images/og-register-to-vote.jpg";
   }
   //define $title for Absentee Ballot Pages with State Names
   else if ($page_type == "absentee-ballot" && $state == "" ) {
-    $title = "Absentee Ballots | Vote.org - The Absentee Ballot Experts";
+    $title = "Absentee Ballots | VOTE.org - The Absentee Ballot Experts";
     $description = "It takes 2 minutes to get your absentee ballot. Get started now.";
     $image = $theme_uri."/dist/images/og-absentee.jpg";
   }
@@ -48,29 +48,29 @@ function hook_meta() {
 
   else if ($page_type == "absentee-ballot" && $state !== "") {
     $state_name = $post->post_title;
-    $title = "$state_name Absentee Ballots | Vote.org - The Absentee Ballot Experts";
+    $title = "$state_name Absentee Ballots | VOTE.org - The Absentee Ballot Experts";
     $description = "It takes 2 minutes to get your $state_name absentee ballot. Get started now.";
     $image = $theme_uri."/dist/images/og-absentee.jpg";
   } 
   //define $title for Election Center Pages
   else if ($page_type == "state" && $state == "" ) {
-    $title = "Election Center | Vote.org";
+    $title = "Election Center | VOTE.org";
     $description = "Everything you need to vote: register to vote, check your registration status, get your absentee ballot.";
     $image = $theme_uri."/dist/images/og-default.jpg";
   }
   //define $title for Election Center Pages with State Names
   else if ($page_type == "state" && $state !== "" ) {
     $state_name = $post->post_title;
-    $title = "$state_name Election Center | Vote.org";
+    $title = "$state_name Election Center | VOTE.org";
     $description = "Everything you need to vote in $state_name: register to vote, check your registration status, get your absentee ballot.";
     $image = $theme_uri."/dist/images/og-default.jpg";
   }
   //homepage  meta tags
   else if ( is_home() || is_front_page() ) {
-    $title = "Vote.org - Everything you need to vote";
+    $title = "VOTE.org - Everything you need to vote";
     $description = get_post_meta($post->ID, "wpcf-meta-description", true);
     if (empty($description)) {
-      $description = "Vote.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
+      $description = "VOTE.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
     }
 
     $image = get_post_meta($post->ID, "wpcf-meta-image", true);
@@ -79,24 +79,24 @@ function hook_meta() {
     }
   } 
   else if ( is_404() ) {
-    $title = "404 Error Page Not Found | Vote.org - Everything you need to vote";
-    $description = "404 Error Page Not Found | Vote.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
+    $title = "404 Error Page Not Found | VOTE.org - Everything you need to vote";
+    $description = "404 Error Page Not Found | VOTE.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
     $image = $theme_uri."/dist/images/og-default.jpg";
 
   } else if ( is_search() ) {
     $query = get_search_query();
-    $title = "Search Results for $query | Vote.org - Everything you need to vote";
-    $description = "Vote.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
+    $title = "Search Results for $query | VOTE.org - Everything you need to vote";
+    $description = "VOTE.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
     $image = $theme_uri."/dist/images/og-default.jpg";
      
   }
   else {
     //all other pages on site are using the defaul description and image. Adding post title to the title tag.
-    $title = $post->post_title." | Vote.org - Everything you need to vote";
+    $title = $post->post_title." | VOTE.org - Everything you need to vote";
     //check to see if a custom meta description is set on the post, if so display that, if not display the default language
     $description = get_post_meta($post->ID, "wpcf-meta-description", true);
     if (empty($description)) {
-      $description = "Vote.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
+      $description = "VOTE.org knows that Americans want to vote, and that there are millions of Americans who want to vote -- and who will vote consistently -- as voting becomes easier and more convenient.";
     }
     //check to see if a custom field meta-image is set on the post, if so display that, if not display the default image
     $image = get_post_meta($post->ID, "wpcf-meta-image", true);
@@ -113,7 +113,7 @@ function hook_meta() {
   $output .= "<meta property=\"og:description\" content=\"$description\" />\r\n";
   $output .= "<meta property=\"og:url\" content=\"$url\">\r\n";
   $output .= "<meta property=\"og:type\" content=\"article\">\r\n";
-  $output .= "<meta property=\"og:site_name\" content=\"Vote.org\">\r\n";
+  $output .= "<meta property=\"og:site_name\" content=\"VOTE.org\">\r\n";
   $output .= "<meta property=\"og:image\" content=\"$image\">\r\n\r\n";
   $output .= "<meta name=\"twitter:site\" content=\"@votedotorg\">\r\n";
   $output .= "<meta name=\"twitter:url\" content=\"$url\">\r\n";
