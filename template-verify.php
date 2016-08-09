49c73e4ba2c3f57a723c39a8e26a0c8089ffbd9a
debracleaver <debracleaver@users.noreply.github.com>
2016-08-09 13:00:25 -0700

    Add share tool
    it's missing. odd.

--- template-verify.php
+++ template-verify.php
@@ -8,4 +8,6 @@
 <?php while (have_posts()) : the_post(); ?>
 
   <?php get_template_part('templates/content', 'verify'); ?>
+  <?php get_template_part('templates/content', 'share'); ?>
+  
 <?php endwhile; ?>
