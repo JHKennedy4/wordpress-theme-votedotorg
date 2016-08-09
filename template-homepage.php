be18973d6230cf25c19a4d5e95c3e8150e884b3f
debracleaver <debracleaver@users.noreply.github.com>
2016-08-09 12:38:58 -0700

    Update template-homepage.php
    cleaning up code to make it consistent with itself.

--- template-homepage.php
+++ template-homepage.php
@@ -7,7 +7,7 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content-hero'); ?>
-  <?php get_template_part('templates/content', 'homepage'); ?>
+  <?php get_template_part('templates/content-homepage'); ?>
   <?php get_template_part('templates/content-share'); ?>
 
 <?php endwhile; ?>
