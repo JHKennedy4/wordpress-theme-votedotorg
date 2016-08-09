c8d8e65290e74bd7ae011b6a9c5f09d6736b682e
debracleaver <debracleaver@users.noreply.github.com>
2016-08-09 12:35:30 -0700

    Adding share links
    This template is inconsistent with the others in that the share links were part of the partial and not the template page

--- template-homepage.php
+++ template-homepage.php
@@ -8,5 +8,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content-hero'); ?>
   <?php get_template_part('templates/content', 'homepage'); ?>
+  <?php get_template_part('templates/content-share'); ?>
 
 <?php endwhile; ?>
