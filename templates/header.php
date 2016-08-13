31b7ad49fb0e95f904b23f44abe2ec4b9aa908e6
debracleaver <debracleaver@users.noreply.github.com>
2016-08-12 20:52:04 -0700

    reorder mobile topnav
    Now it matches the sitemap in the footer, which i think is a nice touch.

--- templates/header.php
+++ templates/header.php
@@ -24,52 +24,61 @@
 
 </nav><!--Top Nav-->
 
+// mobile menu
+
 <div class="mobile-only nav-menu collapsed">
   <ul class="no-dropdown">
-    <li><a href="/register-to-vote/">Register to vote</a></li>
-    <li class="active"><a href="/am-i-registered-to-vote/">Are you registered?</a></li>
-    <li><a href="/absentee-ballot/">Absentee ballots</a></li>
-    <li><a href="/polling-place-locator/">Polling place locator</a></li>
-    <li><a href="http://help.vote.org">Help desk</a></li>
-    <li><a href="/technology">Use our technology for free!</a></li>
     <li>
-      <a class="usa-button usa-button-outline" href="https://secure.actblue.com/contribute/page/votedotorg?refcode=website-top-nav">
+      <a class="usa-button-secondary" href="https://secure.actblue.com/contribute/page/votedotorg?refcode=website-top-nav">
         Donate
       </a>
     </li>
   </ul><!--.no-dropdown-->
   <div class="usa-accordion">
     <ul class="usa-unstyled-list">
-
       <li>
         <button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-1">
-          Election Dates &amp; Deadlines
+          Voter registration
         </button>
         <div id="collapsible-1" aria-hidden="true" class="usa-accordion-content">
           <ul>
-            <li><a href="/early-voting-calendar">Early voting calendar</a></li>
+            <li><a href="/register-to-vote/">Register to vote</a></li>
+            <li><a href="/am-i-registered-to-vote/">Check your registration status</a></li>
             <li><a href="/voter-registration-deadlines">Voter registration deadlines</a></li>
-            <li><a href="/absentee-ballot-deadlines">Absentee ballot deadlines</a></li>
+            <li><a href="/voter-registration-rules">Voter registration rules</a></li>
           </ul>
         </div>
       </li>
       <li>
         <button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-2">
-          Voting Rules
+         Absentee ballots
         </button>
         <div id="collapsible-2" aria-hidden="true" class="usa-accordion-content">
           <ul>
-            <li><a href="/voter-registration-rules">Voter registration rules</a></li>
+            <li><a href="/absentee-ballot/">Get your absentee ballot</a></li>
+            <li><a href="/absentee-ballot-deadlines">Absentee ballot deadlines</a></li>
             <li><a href="/absentee-voting-rules">Absentee voting rules</a></li>
-            <li><a href="/voter-id-laws">Voter ID laws</a></li>
           </ul>
         </div>
       </li>
       <li>
         <button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-3">
-          About Us
+          Voting
         </button>
         <div id="collapsible-3" aria-hidden="true" class="usa-accordion-content">
+          <ul>
+            <li><a href="/voter-id-laws">Voter ID laws</a></li>
+            <li><a href="/early-voting-calendar">Early voting calendar</a></li>
+            <li><a href="/polling-place-locator/">Polling place locator</a></li>
+            <li><a href="http://help.vote.org">Help desk</a></li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-4">
+         About Vote.org
+        </button>
+        <div id="collapsible-4" aria-hidden="true" class="usa-accordion-content">
           <ul>
             <li><a href="/about">About</a></li>
             <li><a href="/team">Team</a></li>
