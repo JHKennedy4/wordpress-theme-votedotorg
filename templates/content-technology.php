2f49d445195547b6b8a348ed3f8f5cbdaa81c12c
debracleaver <debracleaver@users.noreply.github.com>
2016-08-19 00:38:58 -0700

    formatting fixes
    making the iframe code that partners copy fit better in the textarea

--- templates/content-technology.php
+++ templates/content-technology.php
@@ -10,31 +10,20 @@
     
       <h2>Free voter registration tool</h2>
       
-      <textarea id="clipboard_textarea1">
-        <iframe src="https://register.vote.org" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" >
-        </script>
-
-        <script type="text/javascript">
-          iFrameResize({ log:true, checkOrigin:false});
-        </script>
-
-
-      </textarea><!--#clipboard_textarea1-->
-      <button class="clip_button" variant="1" data-clipboard-target="clipboard_textarea1">Click to copy code</button>
+    <textarea id="clipboard_textarea1">
+<iframe src="https://register.vote.org" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>
+<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>
+    </textarea><!--#clipboard_textarea1-->
+    <button class="clip_button" variant="1" data-clipboard-target="clipboard_textarea1">Click to copy code</button>
 
       
       <h2>Free absentee ballot tool</h2>
       
       <textarea id="clipboard_textarea2">
-        <iframe src="https://absentee.vote.org" width="100%" marginheight="0" frameborder="0" id="frame2" scrollable="no">
-        </iframe>
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" >
-        </script>
-
-        <script type="text/javascript">
-          iFrameResize({ log:true, checkOrigin:false});
-        </script>
+  <iframe src="https://absentee.vote.org" width="100%" marginheight="0" frameborder="0" id="frame2" scrollable="no"></iframe>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>
+  <script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>
       </textarea><!--#clipboard_textarea2-->
       <button class="clip_button"  variant="1" data-clipboard-target="clipboard_textarea2">Click to copy code</button>
       
@@ -42,15 +31,9 @@
       <h2>Free check registration status tool</h2>
       
       <textarea id="clipboard_textarea3">
-        <iframe src="https://verify.vote.org" width="100%" marginheight="0" frameborder="0" id="frame3" scrollable="no">
-        </iframe>
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" >
-        </script>
-
-        <script type="text/javascript">
-          iFrameResize({ log:true, checkOrigin:false});
-        </script>
-
+  <iframe src="https://verify.vote.org" width="100%" marginheight="0" frameborder="0" id="frame3" scrollable="no"></iframe>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" ></script>
+  <script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>
       </textarea><!--#clipboard_textarea3-->
       <button class="clip_button"  variant="1"  data-clipboard-target="clipboard_textarea3">Click to copy code</button>
       
