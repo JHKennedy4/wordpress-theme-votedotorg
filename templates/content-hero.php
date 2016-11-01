ad0c077bee22aa25eef13f1818bee46659ff37a0
John Kennedy <jhkennedy4@gmail.com>
2016-10-31 21:15:06 -0700

    more inline styles

--- templates/content-hero.php
+++ templates/content-hero.php
@@ -63,18 +63,18 @@ if (isset($wp_query->query_vars['state_name'])) {
         <!--div class="register-line"></div-->
         <h3>It takes less than 2 minutes to register.</h3>
 
-        <a class="usa-button usa-button-outline-inverse" href="/register-to-vote/<?php if ($state !== "") { echo $state; }?>">Register to vote</a>
+        <a class="usa-button" href="/register-to-vote/<?php if ($state !== "") { echo $state; }?>">Register to vote</a>
       </div><!--.box-->
 
       <div class="box box--light">
         <!--div class="check-registration-line"></div-->
         <h3>Find out if you are registered to vote.</h3>
-        <a class="usa-button usa-button-outline-inverse" href="/am-i-registered-to-vote/">Check registration status</a>
+        <a class="usa-button" href="/am-i-registered-to-vote/">Check registration status</a>
       </div><!--.box-->
       <div class="box box--light">
         <!--div class="mail-line"></div-->
         <h3>Can't vote in person on Election Day?</h3>
-        <a class="usa-button usa-button-outline-inverse" href="/absentee-ballot/<?php if ($state !== "") { echo $state; }?>">Get your absentee ballot</a>
+        <a class="usa-button" href="/absentee-ballot/<?php if ($state !== "") { echo $state; }?>">Get your absentee ballot</a>
       </div><!--.box-->
 
     </div><!--.boxes-->
@@ -87,28 +87,84 @@ section.hero .container {
 }
 
 section.hero .box {
-    padding: 30px !important;
+    margin-bottom: 30px !important;
 }
 
-section.hero .box.box--light {
-    margin-top: 30px !important;
+section.hero .box h2 {
+    text-align: center;
+    margin-bottom: 0;
+    height: auto;
+    font-size: 30px;
+    line-height: 39px;
+    margin-bottom: 30px;
 }
 
-@media screen and (min-width:481px){
-    section.hero .box.box--light .usa-button.usa-button-outline-inverse {
-        font-size: 13px !important;
-    }
+section.hero .box.box--light h3 {
+  text-align: center;
+}
+
+.usa-button.usa-button-secondary {
+    font-size: 20px !important;
 }
 
 @media only screen and (min-width: 768px) {
     section.hero .container {
-       max-width: none !important;
+      max-width: none !important;
+    }
+
+    section.hero .box h2 {
     }
 }
+
+@media only screen and (min-width: 1007px) {
+    section.hero .box h2 {
+    }
+}
+
+@media only screen and (min-width: 1080px) {
+    section.hero .container {
+       max-width: 1080px !important;
+       width: auto !important;
+    }
+}
+
+section.hero .boxes {
+}
+
+section.hero .box {
+    padding: 30px !important;
+}
+
+
+section.hero .box.box--light {
+}
+
+section.hero .box.box--light h3 {
+    font-family: Merriweather;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 26px;
+}
+
+@media screen and (min-width:481px){
+    section.hero .box.box--light .usa-button.usa-button-outline-inverse {
+        font-size: 15px !important;
+    }
+}
+
 
 section.hero .box .usa-button, .usa-button:visited {
     width: 100% !important;
 }
+
+.site-name {
+  margin-bottom: 10px !important;
+}
+.tagline {
+  margin-bottom: 65px !important;
+  font-weight: bold !important;
+}
+
 </style>
 </section>
 
